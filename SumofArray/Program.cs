fda53ba53889c6112a52957a01f0cbc89fed9688
Kunal-Rajput-18 <kunal4248rajput@gmail.com>
2024-10-29 14:52:27 +0530

    Add project files.

---
+++ SumofArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SumofArray;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int[]a=new int[5];
+        int i;
+        int sum = 0;
+        try
+        {
+            Console.WriteLine("Enter no of array elements");
+            for (i = 0; i < a.Length; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            for (i = 0; i < a.Length; i++)
+            {
+                sum = sum + a[i];
+            }
+            Console.WriteLine("Sum of Array is " + sum);
+            Console.ReadLine();
+        } catch (Exception e)
+        {
+            Console.WriteLine("enter valid number "+ e);
+        }
+    }
+}
